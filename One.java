3f8c1fdb92c72fa8eddab67fa5aae92da8dcfd84
Евгений <dog1313@yandex.ru>
2023-11-28 20:54:50 +0300

    constryctor

--- One.java
+++ One.java
@@ -3,4 +3,11 @@ public class One {
     private static String name;
     private static int age;
     private static int phone;
+
+    public One (String name, int age, int phone){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+
+    }
 }
